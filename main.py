a678674284050bdea193c5a455c47468f7f53150
MaksSadkov83 <maksraptor543@gmail.com>
2023-08-17 14:42:22 +0300

    add graph gyroscope

--- main.py
+++ main.py
@@ -11,11 +11,29 @@ Builder.load_file("PitsAndRoads.kv")
 
 
 class GyroscopeScreen(Screen):
-    x_calib = NumericProperty(0)
-    y_calib = NumericProperty(0)
-    z_calib = NumericProperty(0)
+    def __init__(self):
+        super().__init__()
+        self.graph = self.ids.graph_plot
+        self.counter = 0
+
+        # For all X, Y and Z axes
+        self.plot = []
+        self.plot.append(MeshLinePlot(color=[1, 0, 0, 1]))  # X - Red
+        self.plot.append(MeshLinePlot(color=[0, 1, 0, 1]))  # Y - Green
+        self.plot.append(MeshLinePlot(color=[0, 0, 1, 1]))  # Z - Blue
+
+        self.reset_plots()
+
+        for plot in self.plot:
+            self.graph.add_plot(plot)
 
     facade = ObjectProperty()
+
+    def reset_plots(self):
+        for plot in self.plot:
+            plot.points = [(0, 0)]
+
+        self.counter = 1
 
     def enable(self):
         try:
@@ -38,8 +56,21 @@ class GyroscopeScreen(Screen):
             self.ids.accel_status.text = status
 
     def get_rotation(self, dt):
+        if self.counter == 100:
+            # We re-write our points list if number of values exceed 100.
+            # ie. Move each timestamp to the left.
+            for plot in self.plot:
+                del plot.points[0]
+                plot.points[:] = [(i[0] - 1, i[1]) for i in plot.points[:]]
+
+            self.counter = 99
+
         if self.facade.rotation != (None, None, None):
-            self.x_calib, self.y_calib, self.z_calib = self.facade.rotation
+            self.plot[0].points.append((self.counter, self.facade.rotation[0]))
+            self.plot[1].points.append((self.counter, self.facade.rotation[1]))
+            self.plot[2].points.append((self.counter, self.facade.rotation[2]))
+
+        self.counter += 1
 
 
 class AccelerometerScreen(Screen):
